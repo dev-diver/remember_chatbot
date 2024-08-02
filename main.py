8215f5fbd43aaa4df058869808b87da5f8f7bdc8
SoKyungHyun <valentine92exp@gmail.com>
2024-08-02 17:03:00 +0900

    ReAct

--- main.py
+++ main.py
@@ -4,108 +4,52 @@ from openai import OpenAI
 from pprint import pprint
 import time
 
-
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
 model3 = "gpt-3.5-turbo-1106"
 model4 = "gpt-4-0613"
 
-agenda = """
-'인공지능이 인간의 일자리를 위협합니다. 이에 대한 대응 방안을 논의합니다.
-"""
-
-sampling_template = """
-{agenda}에 대해 논의 중입니다.
-```
-[이전 의견]:
-{selected}
-```
-[이전 의견]에 대한 구체적이며 실질적인 구현 방안을 아래 JSON 형식으로 답하세요.
-{{
-    "주제": <주제>
-    "구현": <50단어 이내로 작성하세요>,
-    "근거": <[이전 의견]의 어떤 대목에서 그렇게 생각했는지>
-}}
-"""
-
-evaluation_template="""
-{agenda}에 대해 논의하고 있습니다.
-```
-[의견]:
-{thought}
-```
-위의 [의견]을 아래 JSON 형식으로 평가하세요.
-{{
-    "창의적이고 혁신적인 방법인가" : <15점 만점 기준 점수>,
-    "단기간 내에 실현 가능한 방법인지" : <10점 만점 기준 점수">,
-    "총점": <총점>
-}}
-"""
-
 total_tokens = 0
-
-def request_gpt(message, model, temperature, type="json_object"):
-    message = [{"role":"user", "content":message}]
-    response = client.chat.completions.create(
-        model=model, 
-        messages=message, 
-        temperature=temperature,
-        # response_format={"type": type}
-    ).model_dump()
-    if type == "json_object":
-        response_content = json.loads(response['choices'][0]['message']['content'])
-    else:
-        response_content = response['choices'][0]['message']['content']
-    global total_tokens
-    total_tokens += response['usage']['total_tokens']
-    return response_content
-
-def generate_thoughts(selected):
-    selected = "없음" if len(selected) == 0 else selected
-    samples = []
-    message = sampling_template.format(agenda=agenda, selected=selected)
-    for _ in range(5):
-        sample = request_gpt(message, model3, 1.2)
-        samples.append(sample['구현'])
-    return samples
-
-def evalutate(thoughts):
-    values = []
-    for thought in thoughts:
-        message = evaluation_template.format(agenda=agenda, thought=thought)
-        value = request_gpt(message, model4, 0)
-        values.append({
-            "thought": thought,
-            "value": value
-        })
-        print(f"thoughts: {thought}, totalValue: {value['총점']}")
-    return values
-
-def get_top_n(values, n):
-    return sorted(values, key=lambda x: x['value']['총점'], reverse=True)[:n]
-
-
-selected_list = []
-selected = ""
 
 start_time = time.time()
 
-for step in range(3):
-    thoughts = generate_thoughts(selected)
-    values = evalutate(thoughts)
-    selected = get_top_n(values, 1)[0]['thought']
-    selected_list.append(selected)
-    print(f"{step +1}단계, {selected}")
+template = """
+Thought, Action, Observation 단계를 번갈아가며 질문에 답해가는 과정을 통해 <과제/> 를 해결합니다.
+1. Thought: 현재 상황에 대한 추론
+2. Action
+ - Search[keyword] : <도구상자/> 에서 도구 하나를 꺼내서 keyword 검색
+ - Finish {"해결책": <해결책을 단답형으로 제시하고 작업을 완료>}
+3. Observation: 도구를 사용한 결과를 객관적으로 관찰
+```
+<도구상자>
+ -온도 검색[도시명]: {"서울" :20.1, "자카르타": 32.1, "헬싱키": -1},
+ -입을 옷 검색[더운 날씨, 선선한 날씨, 추운 날씨 등]: {"더운 날씨": "반팔", "선선한 날씨": "긴팔 티셔츠", "추운 날씨": "패딩"}
+</도구상자>
+```
 
-print("\n".join(selected_list))
+- 매회차별 1-SET, 2-SET, ..., N-SET로 표기할 것.
+- 매회차별 필요한 부분을 나누어 생각(Thought)할 것.
+- Action을 출력할 때는 도구명과 keyword를 표기할 것.
+```
+<과제>
+  내일 자카르타로 떠날 예정입니다. 어떤 옷을 챙겨가면 될까요?
+</과제>
+"""
 
-summary = f"{agenda} 다음 내용을 근거로 짧은 보고서를 작성하세요:" + str(selected_list)
-report = request_gpt(summary, model4, 0, type="text")
+context = [{"role": "user", "content": template}]
+response = client.chat.completions.create(
+    model=model4,
+    messages=context,
+    temperature=0,
+    top_p=0
+).model_dump()
+
 
 end_time = time.time()
 execution_time = end_time - start_time
 
-print(report)
+pprint(response['choices'][0]['message']['content'])
+total_tokens += response['usage']['total_tokens']
 print(f"Total tokens: {total_tokens}")
 print(f"Execution time: {execution_time} seconds")
