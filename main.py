8734c558ec5448db416f53320b059c19fb6a1bfa
SoKyungHyun <valentine92exp@gmail.com>
2024-08-02 13:37:53 +0900

    few shot prompting

--- main.py
+++ main.py
@@ -9,32 +9,23 @@ client = OpenAI(api_key=api_key)
 model = "gpt-3.5-turbo-1106"
 
 template = """
-당신은 번역 함수이며, 반환값은 반드시 JSON 데이터여야 합니다.
-STEP별로 작업을 수행하면서 그 결과를 아래의 출력 골과 JSON 포맷에 작성하세요.
-STEP-1. 아래 세 개의 벡틱으로 구분된 텍스트를 원문 그대로 읽어올 것
-STEP-2. 입력받은 텍스트가 영어가 아니라면 false를 표기하고 STEP-3를 진행하지 말 것
-STEP-3. 다음의 말투로 번역할 것:["지구의 나이는 45억 살이야.", "세종대왕은 조선의 위대한 국왕이야."]
-```{text}```
----
-출력결과: {{"STEP-1" <입력텍스트>, "STEP-2": <true/false>, "STEP-3": <번역결과>}}
+긍정 또는 부정으로 답변을 작성하세요.
+Q: {text}
+A: 
 """
 
-text = "William Shakespeare was an English playwright, poet, and actor, He is widely regarded as the greatest writer in the English language and the world's pre-eminent dramatist."
-
-
+text = "매력적인 이성과 사랑에 빠졌어!"
 template = template.format(text=text)
-pprint(template)
 
 context = [
     {"role": "user", "content": template},
 ]
 
 response =  client.chat.completions.create(
-    model=model, 
+    model="gpt-4-0613", 
     messages=context,
     temperature = 0,
     top_p=0,
-    seed=1234).model_dump()
+    ).model_dump()
 
 pprint(response)
-pprint(json.loads(response['choices'][0]['message']['content']))
