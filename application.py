6930afafd42181c5308b4e66af5e1eda4ba23178
SoKyungHyun <valentine92exp@gmail.com>
2024-08-05 12:30:15 +0900

    chat api

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
@@ -9,6 +9,11 @@ def hello():
 @application.route('/chat-app')
 def chat_app():
     return render_template('chat.html')
+
+@application.route('/chat-api', methods=['POST'])
+def chat_api():
+    print("request.json:", request.json)
+    return {"response_message": "나도" + request.json['request_message']}
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port=8080)
